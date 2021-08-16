c3b58df81c863a79acf003d795d56f734008095c
Hashem <PersonalHashem@gmail.com>
2021-08-17 00:36:02 +0430

    feat: create linkedlist stack

--- hashem/stack.py
+++ hashem/stack.py
@@ -27,3 +27,36 @@ class AbstractStack(metaclass=ABCMeta):
     @abstractmethod
     def peek(self):
         pass
+
+    def is_empty(self):
+        return self._size == 0
+
+
+class StackNode:
+    __slots__ = ['item', 'next']
+
+    def __init__(self, item, next=None):
+        self.item = item
+        self.next = next
+
+
+class LinkedListStack(AbstractStack):
+    def __init__(self):
+        super().__init__()
+        self._top = None
+
+    def push(self, item):
+        self._top = StackNode(item, self._top)
+        self._size += 1
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            item = self._top.item
+            self._top = self._top.next
+            self._size -= 1
+            return item
+
+    def peek(self):
+        return self._top.item if self._top else None
