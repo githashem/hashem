3fc5ffc256ec998fd3e8985b2da9933520a2ded8
githashem <PersonalHashem@gmail.com>
2021-10-01 06:38:29 +0330

    fix(queue): Fix a bug in 'front' and 'rear' property

--- hashem/queue.py
+++ hashem/queue.py
@@ -5,6 +5,8 @@ class AbstractQueue(ABC):
     """ Create a new queue that is empty """
     def __init__(self):
         self._size = 0
+        self._front = None
+        self._rear = None
 
     def __len__(self):
         return self._size
@@ -15,16 +17,18 @@ class AbstractQueue(ABC):
         return self._size
 
     @property
-    @abstractmethod
     def front(self):
         """ Return the front element of the queue """
-        pass
+        if self.is_empty():
+            return None
+        return self._front.item
 
     @property
-    @abstractmethod
     def rear(self):
         """ Return the rear element of the queue """
-        pass
+        if self.is_empty():
+            return None
+        return self._rear.item
 
     @abstractmethod
     def enqueue(self, item):
@@ -36,14 +40,15 @@ class AbstractQueue(ABC):
         """ Remove the front item from the queue """
         pass
 
-    @abstractmethod
     def clear(self):
         """ Remove all items from the queue """
-        pass
+        self._size = 0
+        self._front = None
+        self._rear = None
 
     def is_empty(self):
         """ Return True if the queue is empty, False otherwise """
-        return self._size == 0
+        return self._size == 0 and self._front is None and self._rear is None
 
 
 class QueueNode:
@@ -56,11 +61,6 @@ class QueueNode:
 
 
 class LinkedListQueue(AbstractQueue):
-    def __init__(self):
-        super().__init__()
-        self._front = None
-        self._rear = None
-
     def enqueue(self, item):
         if self.is_empty():
             self._front = self._rear = QueueNode(item)
@@ -76,14 +76,3 @@ class LinkedListQueue(AbstractQueue):
             self._front = self._front.next
             self._size -= 1
             return item
-
-    def clear(self):
-        self._size = 0
-        self._front = None
-        self._rear = None
-
-    def front(self):
-        return self._front.item
-
-    def rear(self):
-        return self._rear.item
